d6d78c454f99b46c087b9002a996049084a08e18
Nise Roj <b.korojo@gmail.com>
2022-07-27 16:27:46 +0100

    Patch 30

--- sketch.js
+++ sketch.js
@@ -10,6 +10,27 @@
 
 let cells = []; // array of cells objects
 let cells1 = [];
+
+//3D SPECTACULE
+let v = [];
+let rows = 60,
+  cols = 120;
+
+let canvas;
+
+let pNumSlider, pLenSlider, diameterSlider, pSharpSlider;
+let petalNum, pLength, diameter, pSharpness;
+
+let heightSlider, curvatureSlider1, curvatureSlider2;
+let flowerHeight, curvature1, curvature2;
+
+let bumpSlider, bumpNumSlider;
+let bump, bumpNum;
+
+let pNum, fD, pLen, pSharp;
+let fHeight, curve1, curve2;
+let b, bNum;
+
 /**
  * Initialise the cells array with a number of new Cell objects
  *
@@ -170,7 +191,7 @@ function handleLife(cellsArray) {
 
 function mitosis(cellsArray) {
   for (let i = 0; i < cellsArray.length; i++) {
-    if (cellsArray[i].getLife() < 10) {
+    if (cellsArray[i].getLife() == 0) {
       probability = floor(random(0, 100));
       if (probability <= 55) {
         let CellA = new Cell({
@@ -187,6 +208,14 @@ function mitosis(cellsArray) {
       cellsArray.splice(i, 1);
     }
   }
+}
+
+function vShape(A, r, a, b, c) {
+  return A * pow(Math.E, -b * pow(abs(r), c)) * pow(abs(r), a);
+}
+
+function bumpiness(A, r, f, angle) {
+  return 1 + A * pow(r, 2) * sin(f * angle);
 }
 
 function setup() {
@@ -208,6 +237,56 @@ function setup() {
 
   cells1 = createCellsArray(40, cells1);
   console.log(cells1);
+
+  //3D SPECTACLE
+  // colorMode(HSB, 360, 100, 100);
+  angleMode(DEGREES);
+  noStroke();
+
+  petalNum = createDiv();
+  petalNum.class("valueDisplay");
+  pNumSlider = createSlider(1, 20, 5, 1);
+  pNumSlider.class("Slider");
+
+  diameter = createDiv();
+  diameter.class("valueDisplay");
+  diameterSlider = createSlider(20, 250, 200, 10);
+  diameterSlider.class("Slider");
+
+  pLength = createDiv();
+  pLength.class("valueDisplay");
+  pLenSlider = createSlider(0, 300, 60, 10);
+  pLenSlider.class("Slider");
+
+  pSharpness = createDiv();
+  pSharpness.class("valueDisplay");
+  pSharpSlider = createSlider(0.0, 10.0, 0.4, 0.1);
+  pSharpSlider.class("Slider");
+
+  flowerHeight = createDiv();
+  flowerHeight.class("valueDisplay");
+  heightSlider = createSlider(0, 600, 300, 10);
+  heightSlider.class("Slider");
+
+  curvature1 = createDiv();
+  curvature1.class("valueDisplay");
+  curvatureSlider1 = createSlider(0.0, 4.0, 0.8, 0.1);
+  curvatureSlider1.class("Slider");
+
+  curvature2 = createDiv();
+  curvature2.class("valueDisplay");
+  curvatureSlider2 = createSlider(0.0, 1.0, 0.2, 0.05);
+  curvatureSlider2.class("Slider");
+
+  bump = createDiv();
+  bump.class("valueDisplay");
+  bumpSlider = createSlider(0.0, 5.0, 2.5, 0.5);
+  bumpSlider.class("Slider");
+
+  bumpNum = createDiv();
+  bumpNum.class("valueDisplay");
+  bumpNumSlider = createSlider(0, 20, 10, 1);
+  bumpNumSlider.class("Slider");
 }
 
 ///----------------------------------------------------------------------------
@@ -216,7 +295,7 @@ function setup() {
 function draw() {
   orbitControl(); // camera control using mouse
 
-  //lights(); // we're using custom lights here
+  // lights(); // we're using custom lights here
   directionalLight(180, 180, 180, 0, 0, -width / 2);
   directionalLight(255, 255, 255, 0, 0, width / 2);
 
@@ -227,11 +306,86 @@ function draw() {
   fill(220);
   ambientMaterial(80, 202, 94); // magenta material
 
+  // 3D SPECTACULE
+  orbitControl(4, 4);
+
+  rotateX(60);
+
+  pNum = pNumSlider.value();
+  fD = diameterSlider.value();
+  pLen = pLenSlider.value();
+  pSharp = pSharpSlider.value();
+
+  fHeight = heightSlider.value();
+  curve1 = curvatureSlider1.value();
+  curve2 = curvatureSlider2.value();
+
+  b = bumpSlider.value();
+  bNum = bumpNumSlider.value();
+
+  for (theta = 0; theta < rows; theta += 1) {
+    v.push([]);
+    for (let phi = 0; phi < cols; phi += 1) {
+      let r =
+        ((pLen * pow(abs(sin(((pNum / 2) * phi * 360) / cols)), pSharp) + fD) *
+          theta) /
+        rows;
+      let x = r * cos((phi * 360) / cols);
+      let y = r * sin((phi * 360) / cols);
+      let z =
+        vShape(fHeight, r / 100, curve1, curve2, 1.5) -
+        200 +
+        bumpiness(b, r / 100, bNum, (phi * 360) / cols);
+
+      let pos = createVector(x, y, z);
+      v[theta].push(pos);
+    }
+  }
+
+  for (let theta = 0; theta < v.length; theta++) {
+    fill(340, 100 - theta, 100);
+    for (let phi = 0; phi < v[theta].length; phi++) {
+      if (theta < v.length - 1 && phi < v[theta].length - 1) {
+        beginShape();
+        vertex(v[theta][phi].x, v[theta][phi].y, v[theta][phi].z);
+        vertex(v[theta + 1][phi].x, v[theta + 1][phi].y, v[theta + 1][phi].z);
+        vertex(
+          v[theta + 1][phi + 1].x,
+          v[theta + 1][phi + 1].y,
+          v[theta + 1][phi + 1].z
+        );
+        vertex(v[theta][phi + 1].x, v[theta][phi + 1].y, v[theta][phi + 1].z);
+        endShape(CLOSE);
+      } else if (theta < v.length - 1 && phi == v[theta].length - 1) {
+        beginShape();
+        vertex(v[theta][phi].x, v[theta][phi].y, v[theta][phi].z);
+        vertex(v[theta][0].x, v[theta][0].y, v[theta][0].z);
+        vertex(v[theta + 1][0].x, v[theta + 1][0].y, v[theta + 1][0].z);
+        vertex(v[theta + 1][phi].x, v[theta + 1][phi].y, v[theta + 1][phi].z);
+        endShape(CLOSE);
+      }
+    }
+  }
+
+  petalNum.html("Number of the petals: " + pNumSlider.value());
+  diameter.html("Diameter: " + diameterSlider.value());
+  pLength.html("Petal length: " + pLenSlider.value());
+  pSharpness.html("Petal sharpness: " + pSharpSlider.value());
+
+  flowerHeight.html("Flower height: " + heightSlider.value());
+  curvature1.html("Curvature 1: " + curvatureSlider1.value());
+  curvature2.html("Curvature 2: " + curvatureSlider2.value());
+
+  bump.html("Bumpiness: " + bumpSlider.value());
+  bumpNum.html("Bumpiness number: " + bumpNumSlider.value());
+
+  v = [];
+
   handleLife(cells);
   collideCells(cells, 1); // handle collisions
   constrainCells(cells, createVector(0, 0, 0), width); // keep cells in the world
   drawCells3D(cells); // draw the cells
-  mitosis(cells);
+  //mitosis(cells);
 
   fill(300);
   ambientMaterial(255, 0, 255);
